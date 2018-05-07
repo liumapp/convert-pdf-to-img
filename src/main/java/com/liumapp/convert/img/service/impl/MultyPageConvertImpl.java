50c6a8eafd8664ec26520db39ccbcba9cf8efe7d
liumapp <liumapp.com@gmail.com>
2018-05-07 15:12:20 +0800

    ready to add multy page convert

---
+++ src/main/java/com/liumapp/convert/img/service/impl/MultyPageConvertImpl.java
@@ -0,0 +1,22 @@
+package com.liumapp.convert.img.service.impl;
+
+import com.liumapp.convert.img.service.MultyPageConvertService;
+import org.icepdf.core.pobjects.Document;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liumapp
+ * @file MultyPageConvertImpl.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/7/18
+ */
+@Service
+public class MultyPageConvertImpl implements MultyPageConvertService {
+
+    @Override
+    public void ConvertMultyPage(Document document) {
+
+    }
+
+}
