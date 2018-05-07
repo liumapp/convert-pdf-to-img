241d32494461f5e3acf572eb56eda0a3c5cb85d5
liumapp <liumapp.com@gmail.com>
2018-05-07 14:38:14 +0800

    config export img path

---
+++ src/main/java/com/liumapp/convert/img/config/PatternConfig.java
@@ -0,0 +1,14 @@
+package com.liumapp.convert.img.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liumapp
+ * @file PatternConfig.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/7/18
+ */
+@Configuration
+public class PatternConfig {
+}
--- src/main/java/com/liumapp/convert/img/customer/ImgConverterQueueHandler.java
+++ src/main/java/com/liumapp/convert/img/customer/ImgConverterQueueHandler.java
@@ -2,11 +2,18 @@ package com.liumapp.convert.img.customer;
 
 import com.alibaba.fastjson.JSON;
 import com.liumapp.convert.img.pattern.PdfPattern;
+import com.liumapp.convert.img.service.SinglePageConvertService;
+import org.icepdf.core.exceptions.PDFException;
+import org.icepdf.core.exceptions.PDFSecurityException;
+import org.icepdf.core.pobjects.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author liumapp
@@ -21,11 +28,26 @@ public class ImgConverterQueueHandler {
 
     private static Logger logger = LoggerFactory.getLogger(ImgConverterQueueHandler.class);
 
+    @Autowired
+    private SinglePageConvertService singlePageConvertService;
+
     @RabbitHandler
     public void process (String msg) {
         logger.info("get msg from img-converter-queue , the msg is : \n " + msg + "\n");
         PdfPattern pdfPattern = JSON.parseObject(msg, PdfPattern.class);
-        
+        Document document = new Document();
+
+        try {
+            document.setFile(pdfPattern.getPath());
+        } catch (PDFException e) {
+            e.printStackTrace();
+        } catch (PDFSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        singlePageConvertService.convertFirstPage(document);
     }
 
 }
