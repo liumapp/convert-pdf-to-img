ba6fb799730a53841de3da636dc88eba8563c0a3
liumapp <liumapp.com@gmail.com>
2018-05-07 08:50:41 +0800

    conf sc pom

---
+++ src/main/java/com/liumapp/convert/img/config/RabbitConf.java
@@ -0,0 +1,20 @@
+package com.liumapp.convert.img.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author liumapp
+ * @file RabbitConf.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/7/18
+ */
+@Configuration
+public class RabbitConf {
+
+    @Bean
+    public Queue
+
+}
