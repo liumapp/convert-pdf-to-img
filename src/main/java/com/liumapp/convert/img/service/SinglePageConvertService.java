9794107e6b7cc9b355a6d9c5082088618ba674ec
liumapp <liumapp.com@gmail.com>
2018-05-07 10:55:32 +0800

    ready to add detail service

---
+++ src/main/java/com/liumapp/convert/img/service/SinglePageConvertService.java
@@ -0,0 +1,14 @@
+package com.liumapp.convert.img.service;
+
+/**
+ * @author liumapp
+ * @file SinglePageConvertService.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/7/18
+ */
+public interface SinglePageConvertService {
+
+
+
+}
---
+++ src/main/java/com/liumapp/convert/img/service/impl/SinglePageConvertImpl.java
@@ -0,0 +1,18 @@
+package com.liumapp.convert.img.service.impl;
+
+import com.liumapp.convert.img.service.SinglePageConvertService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liumapp
+ * @file SinglePageConvertImpl.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/7/18
+ */
+@Service
+public class SinglePageConvertImpl implements SinglePageConvertService {
+
+
+
+}
