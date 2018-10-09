d4d84b6478db3a030a8e3964deb196706c70c8aa
liumapp <liumapp.com@gmail.com>
2018-10-09 15:33:56 +0800

    add single page and all page converter service

---
+++ src/main/java/com/liumapp/convert/img/service/AllPageConverter.java
@@ -0,0 +1,20 @@
+package com.liumapp.convert.img.service;
+
+import com.liumapp.convert.img.PageConverter;
+
+/**
+ * file AllPageConverter.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/10/9
+ */
+public class AllPageConverter implements PageConverter {
+
+    @Override
+    public boolean convert() {
+        return false;
+    }
+
+}
---
+++ src/main/java/com/liumapp/convert/img/service/SinglePageConverter.java
@@ -0,0 +1,20 @@
+package com.liumapp.convert.img.service;
+
+import com.liumapp.convert.img.PageConverter;
+
+/**
+ * file SinglePageConverter.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/10/9
+ */
+public class SinglePageConverter implements PageConverter {
+
+    @Override
+    public boolean convert() {
+        return false;
+    }
+
+}
