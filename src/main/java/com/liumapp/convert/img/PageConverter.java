1835f2dc4793202bd68fc2b322cfef91a41c83b6
liumapp <liumapp.com@gmail.com>
2018-10-09 15:20:56 +0800

    add page converter interface

---
+++ src/main/java/com/liumapp/convert/img/PageConverter.java
@@ -0,0 +1,15 @@
+package com.liumapp.convert.img;
+
+/**
+ * file PageConverter.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/10/9
+ */
+public interface PageConverter {
+
+    public boolean convert ();
+
+}
